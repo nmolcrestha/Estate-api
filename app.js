2f9b40da6f16db8021d3e5b6c3f9b65f757771fe
anmolcnc <anmol@curvesncolors.ch>
2024-06-05 15:47:46 +0545

    install npm cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRouter from "./routes/post.route.js";
 import authRouter from "./routes/auth.route.js";
 
 const app = express();
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
