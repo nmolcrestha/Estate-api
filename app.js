bb111ea19d6a63d03c3c198e0afc4c16719f539b
Anmol Shrestha <nmol.crestha@gmail.com>
2024-09-11 09:58:31 +0545

    test

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRouter from "./routes/post.route.js";
 import authRouter from "./routes/auth.route.js";
+import testRouter from "./routes/test.route.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
+app.use("/api/test", testRouter);
 
 app.listen(8800, () => {
   console.log("Server is running");
